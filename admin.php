4a9c0fa001d875ad407cae859888ee943f6adbcf
mingzepeng <mingzepeng@sina.com>
2013-12-18 23:36:03 +0800

    完成后台页面稿

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+include 'init.php';
+
+define('MODULE','admin');
+
+define('CONTROLLER',(isset($_GET['c']) && $_GET['c'] !=='')? trim($_GET['c']) : Config::get('DEFAULT_CONTROLLER'));
+
+define('ACTION',(isset($_GET['a']) && $_GET['a'] !=='')? trim($_GET['a']) : 'index');
+
+App::run(MODULE,CONTROLLER,ACTION);
